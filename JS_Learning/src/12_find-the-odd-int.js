2aa21826b6169c5e7b4c9d04d8e12e9e72e3c956
s3nsh1-dev <shubham228pandey@gmail.com>
2025-06-01 23:59:54 +0530

    dsffd

--- JS_Learning/src/12_find-the-odd-int.js
+++ JS_Learning/src/12_find-the-odd-int.js
@@ -5,24 +5,6 @@
  */
 
 function findOdd(A) {
-  //happy coding!
-  const setA = new Set(A);
-  const mapA = new Map();
-  for (let i of setA) {
-    mapA.set(i, 0);
-  }
-  for (let i of A) {
-    if (mapA.has(i)) {
-      let gg = mapA.get(i);
-      mapA.set(i, gg + 1);
-    }
-  }
-  let result = 0;
-  for (let [key, values] of mapA.entries()) {
-    if (values % 2 != 0) {
-      return key;
-    }
-  }
-  return 0;
+  return A.reduce((acc, num) => acc ^ num, 0);
 }
 module.exports = findOdd;
